f35c01799c9aadc0398b0b7b6652dcb8148980f6
Petros Efthymiou <petrose@backbase.com>
2020-10-13 19:38:54 +0300

    completed repository implementation

---
+++ app/src/main/java/petros/efthymiou/groovy/playlist/PlaylistService.kt
@@ -0,0 +1,10 @@
+package petros.efthymiou.groovy.playlist
+
+import kotlinx.coroutines.flow.Flow
+
+class PlaylistService {
+
+    suspend fun fetchPlaylists() : Flow<Result<List<Playlist>>> {
+        TODO("Not yet implemented")
+    }
+}
