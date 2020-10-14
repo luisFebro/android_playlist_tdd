b067fd54946e38de9a4a68631c5162a7f9663df9
Petros Efthymiou <petrose@backbase.com>
2020-10-14 10:13:09 +0300

    service layer completed

---
+++ app/src/main/java/petros/efthymiou/groovy/playlist/PlaylistAPI.kt
@@ -0,0 +1,8 @@
+package petros.efthymiou.groovy.playlist
+
+interface PlaylistAPI {
+
+    suspend fun fetchAllPlaylists() : List<Playlist> {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/petros/efthymiou/groovy/playlist/PlaylistService.kt
+++ app/src/main/java/petros/efthymiou/groovy/playlist/PlaylistService.kt
@@ -1,10 +1,19 @@
 package petros.efthymiou.groovy.playlist
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import java.lang.RuntimeException
 
-class PlaylistService {
+class PlaylistService (
+    private val api: PlaylistAPI
+) {
 
     suspend fun fetchPlaylists() : Flow<Result<List<Playlist>>> {
-        TODO("Not yet implemented")
+        return flow {
+            emit(Result.success(api.fetchAllPlaylists()))
+        }.catch {
+            emit(Result.failure(RuntimeException("Something went wrong")))
+        }
     }
 }
